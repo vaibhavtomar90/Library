891e40e6d9be3ca471f385acadc8d46a6c53e066
yogesh nachnani <yogesh.nachnani@flipkart.com>
2015-04-20 10:16:24 +0530

    added barebones

---
+++ service/src/main/java/flipkart/pricing/apps/kaizen/App.java
@@ -0,0 +1,13 @@
+package flipkart.pricing.apps.kaizen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
