cc75dde395085309691d0fe52fae4c19020c42c8
yogesh nachnani <yogesh.nachnani@flipkart.com>
2015-04-28 10:23:08 +0530

    Added migrations support via DW migrate

--- service/src/main/java/flipkart/pricing/apps/kaizen/boot/KaizenBooter.java
+++ service/src/main/java/flipkart/pricing/apps/kaizen/boot/KaizenBooter.java
@@ -2,8 +2,9 @@ package flipkart.pricing.apps.kaizen.boot;
 
 import flipkart.pricing.apps.kaizen.boot.bundles.SpringBundle;
 import flipkart.pricing.apps.kaizen.boot.config.KaizenConfiguration;
-import flipkart.pricing.apps.kaizen.boot.healthcheck.RotationStatusHealthCheck;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -20,6 +21,12 @@ public class KaizenBooter extends Application<KaizenConfiguration>
     public void initialize(Bootstrap<KaizenConfiguration> bootstrap) {
         super.initialize(bootstrap);
         bootstrap.addBundle(new SpringBundle());
+        bootstrap.addBundle(new MigrationsBundle<KaizenConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(KaizenConfiguration kaizenConfiguration) {
+                return kaizenConfiguration.getDataSourceFactory();
+            }
+        });
     }
 
     @Override
--- service/src/main/java/flipkart/pricing/apps/kaizen/boot/config/KaizenConfiguration.java
+++ service/src/main/java/flipkart/pricing/apps/kaizen/boot/config/KaizenConfiguration.java
@@ -1,6 +1,11 @@
 package flipkart.pricing.apps.kaizen.boot.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 /**
  * @understands DW Config class for Kaizen App
@@ -8,4 +13,14 @@ import io.dropwizard.Configuration;
 public class KaizenConfiguration extends Configuration {
     // This is the only point of coupling between debian scripts that start the App passing this config to DW & the spring components that read this config
     public static final String YML_CONFIG_FILE = "kaizen-configuration.yml";
+
+    @Valid
+    @NotNull
+    @JsonProperty
+    private DataSourceFactory database = new DataSourceFactory();
+
+    public DataSourceFactory getDataSourceFactory() {
+        database.build(null,null);
+        return database;
+    }
 }
