3a36178ce646525d21b786c5a4c19eaddedc0dc7
Souvik Mitra <souvik.mitra@flipkart.com>
2015-05-05 00:14:08 +0530

    an attempt to wire up hibernate

--- service/src/main/java/flipkart/pricing/apps/kaizen/boot/KaizenBooter.java
+++ service/src/main/java/flipkart/pricing/apps/kaizen/boot/KaizenBooter.java
@@ -2,8 +2,12 @@ package flipkart.pricing.apps.kaizen.boot;
 
 import flipkart.pricing.apps.kaizen.boot.bundles.SpringBundle;
 import flipkart.pricing.apps.kaizen.boot.config.KaizenConfiguration;
+import flipkart.pricing.apps.kaizen.db.domain.ListingInfo;
+import flipkart.pricing.apps.kaizen.db.domain.Signal;
+import flipkart.pricing.apps.kaizen.db.domain.SignalDataTypes;
 import io.dropwizard.Application;
 import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -21,6 +25,12 @@ public class KaizenBooter extends Application<KaizenConfiguration>
     public void initialize(Bootstrap<KaizenConfiguration> bootstrap) {
         super.initialize(bootstrap);
         bootstrap.addBundle(new SpringBundle());
+        bootstrap.addBundle(new HibernateBundle<KaizenConfiguration>(ListingInfo.class, Signal.class, SignalDataTypes.class) {
+            @Override
+            public DataSourceFactory getDataSourceFactory(KaizenConfiguration kaizenConfiguration) {
+                return kaizenConfiguration.getDataSourceFactory();
+            }
+        });
         bootstrap.addBundle(new MigrationsBundle<KaizenConfiguration>() {
             @Override
             public DataSourceFactory getDataSourceFactory(KaizenConfiguration kaizenConfiguration) {
