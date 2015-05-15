4bff2ab6f13ad19bec8b9c974e5f7d2814170e64
yogesh nachnani <yogesh.nachnani@flipkart.com>
2015-05-15 12:07:59 +0530

    removing unnecessary exclude filters

--- service/src/main/java/flipkart/pricing/apps/kaizen/boot/config/KaizenContextConfiguration.java
+++ service/src/main/java/flipkart/pricing/apps/kaizen/boot/config/KaizenContextConfiguration.java
@@ -1,21 +1,13 @@
 package flipkart.pricing.apps.kaizen.boot.config;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
-
-import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
-import java.util.Properties;
 
 /**
  * @understands : Spring context configuration
  */
 @Configuration
-@ComponentScan(value = {"flipkart.pricing.apps.kaizen"}, excludeFilters = {@ComponentScan.Filter(value = {DataSourceConfiguration.class}, type = FilterType.ASSIGNABLE_TYPE)})
+@ComponentScan(value = {"flipkart.pricing.apps.kaizen"})
 @Import({DataSourceConfiguration.class,HibernateConfiguration.class})
 public class KaizenContextConfiguration {
     @Deprecated // for CGLIB only
